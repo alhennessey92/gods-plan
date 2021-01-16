eb7ea9e3fa801262ab9e8d205238354eb748a6a7
Al Hennessey <alhennessey92@gmail.com>
2021-01-16 16:58:17 +0000

    init

---
+++ hermes/src/trading_ig_config.py
@@ -0,0 +1,6 @@
+class config(object):
+    username = "alhennessey92"
+    password = "dabXY654@"
+    api_key = "242ba2b4735e10fc44fd5438e3192e74d26de740"
+    acc_type = "DEMO"  # LIVE / DEMO
+    acc_number = "Z2UDF"
